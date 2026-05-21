9680703128b49830a44a51e7e66fb3357ec6293b
ssumoss <sumeyyevar2@gmai.com>
2026-05-21 09:30:31 +0300

    login api url fix

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://crm-backend-s1h3.onrender.com";
 
 const body = document.body;
 const themeToggle = document.getElementById("themeToggle");
