4f51846e99a3c0d01fc0121d084e8fc1105677b6
ssumoss <sumeyyevar2@gmai.com>
2026-05-21 03:53:55 +0300

    database url fallback

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL = mysql+pymysql://root:şifre@host:port/railway")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Render env okumazsa geçici olarak direkt URL'yi kullan
+if not DATABASE_URL:
+    DATABASE_URL = "mysql+pymysql://root:gBsqYkNiBicgGhxYVioMNPRBpwVrJXkQ@crossover.proxy.rlwy.net:55464/railway"
 
 engine = create_engine(DATABASE_URL)
 
